62efdb99645e5d76ebf98438d025468abf87080e
ncode <proncode@gmail.com>
2022-05-13 15:55:21 +0100

    fixed paystack

---
+++ ecomm/info.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'django-insecure-sj@q!dl(b$iy!97p$=yl2*qkr!6zmd(bgjd0u0an*d2hrfg*e('
--- ecomm/settings.py
+++ ecomm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+from . info import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sj@q!dl(b$iy!97p$=yl2*qkr!6zmd(bgjd0u0an*d2hrfg*e('
-
+SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['54.234.69.57 ','*']
+ALLOWED_HOSTS = ['54.234.69.57','*']
 
 
 # Application definition
